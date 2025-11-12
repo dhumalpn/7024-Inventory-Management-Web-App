ed64cd9738413719ace26bf34e8ab5321f23156e
shankunu <shankunu@mail.uc.edu>
2025-11-12 08:11:46 -0500

    Update navbar

---
+++ InventoryManagement/InventoryManagement/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace InventoryManagement.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string UPC { get; set; } = string.Empty; // Universal Product Code
+        public string Title { get; set; } = string.Empty;
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? Category { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Inventory>? Inventories { get; set; }
+    }
+}
