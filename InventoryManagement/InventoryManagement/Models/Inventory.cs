efc89d6bdc5de058221db8aa0ce95c9f046ad879
shankunu <shankunu@mail.uc.edu>
2025-11-12 07:25:41 -0500

    Add Inventory Model to Project

---
+++ InventoryManagement/InventoryManagement/Models/Inventory.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagement.Models
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = default!;
+        public int Quantity { get; set; }
+        public string? Location { get; set; }
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    }
+}
