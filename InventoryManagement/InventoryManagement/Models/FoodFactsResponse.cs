5b4977dbe13e21dfc1a80311782e034bd6aeb542
Rohan Barai <rohanbarai222@gmail.com>
2025-12-01 13:30:36 -0500

    Added Model Class to map Open Food Facts API Response

---
+++ InventoryManagement/InventoryManagement/Models/FoodFactsResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace InventoryManagement.Models
+{
+    using Newtonsoft.Json;
+
+    public partial class FoodFactsResponse
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("product")]
+        public ProductDetails Product { get; set; }
+
+        [JsonProperty("status")]
+        public long Status { get; set; }
+
+        [JsonProperty("status_verbose")]
+        public string StatusVerbose { get; set; }
+    }
+
+    public partial class ProductDetails
+    {
+        [JsonProperty("nutriscore_grade")]
+        public string NutriscoreGrade { get; set; }
+    }
+
+    public partial class FoodFactsResponse
+    {
+        public static FoodFactsResponse FromJson(string json) => JsonConvert.DeserializeObject<FoodFactsResponse>(json, InventoryManagement.Models.Converter.Settings);
+    }
+}
